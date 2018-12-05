9609a58f5cfbca897f68e5b064e5d9b5a86affa0
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-05 17:43:21 +0800

    feat : add POST mehod

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import Todos from '../containers/TodoContainer.js'
 import TodoInputContainer from '../containers/TodoInputContainer.js'
 import TodoContainer from '../containers/TodoContainer.js'
 
 export default class TodoList extends Component {
-
+  
   render() {
     return (
       <div>
--- src/containers/TodoInputContainer.js
+++ src/containers/TodoInputContainer.js
@@ -4,10 +4,22 @@ import TodoInput from '../components/TodoInput.js'
 
 const mapDispatchToPros = (dispatch) => ({
     addNewToDo : newTodo =>{
-        dispatch({
-          type : "ADDNEWTODO",
-          payload : newTodo
-        })
+        const newTodoItem ={
+            content : newTodo,
+            status : "active"
+        }
+        fetch("http://localhost:8080/api/todos", {method: 'POST', headers: new Headers({
+            'Content-Type': 'application/json'}), 
+            mode: 'cors', 
+            body: JSON.stringify(newTodoItem)})
+            .then(res => res.json())
+            .then(({id, status, content})=>{
+                dispatch({
+                    type : "ADDNEWTODO",
+                    payload : {id, status, content}
+                })
+  })
+        
     }
 })
 
